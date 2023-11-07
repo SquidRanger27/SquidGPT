e78a26850c6adc32a03d683edf663b7756fb29b8
Andrew Dunlop <andrew.w.dunlop@gmail.com>
2023-11-07 20:37:44 +1300

    testing git shortcut

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 // -- imports, requirements, etc:
-require('dotenv').config()
-const { Client, IntentsBitField } = require('discord.js')
-const axios = require('axios')
+import { config } from 'dotenv'
+import { Client, IntentsBitField } from 'discord.js'
+import * as axios from 'axios'
+import express from 'express'
 
 // -- Go the fuck to sleep
 
 // console.log(process.env)
 
-if (process.env.VERSION !== '123') {
-  process.exit(0)
-}
+// if (process.env.VERSION !== '123') {
+//   process.exit(0)
+// }
 
 // -- Wakeup ping (Thanks Tristan):
-const express = require('express')
+config()
+
 const app = express()
 
 app.use(express.static('public'))
@@ -42,14 +44,6 @@ client.on('ready', () => {
 //-- message create:
 
 client.on('messageCreate', async (message) => {
-  const settings = {
-    headerPrompt:
-      'You are a header generator chatbot. You summerise text sent by the user into a concise, simple, and neutral half sentence to be used as a topic header for the message. The header is always less than 35 characters. The header is general, not specific. The header should not be wrapped in any quotations. The header does not try to answer the question or text.',
-    assistantPrompt:
-      'You are an assistant discord chatbot. You provide clear and concise responses, to the users questions and queries.',
-    modelChoicePrompt:
-      'You are gpt-4. A highly advanced and intelligent AI GPT. You task is to evaluate how important and specific a question from the user is, and output a single number value between 0 and 1, where 0 is simple and 1 is complex. You should not answer the user.\nFactors that you should take in to account:\n- If the topic is specific, the value to should be closer to 1\n- If the topic is general knowlege related, the answer should be closer to 0\n- If the topic has little information about it on the internet, the value should be closer to 1\n- If the topic is a well known idea, it should be closer to 0\n- If the user explicity asks for gpt4 the value should be 1. \n- If the user explicity asks for gpt3 the value should be 0.',
-  }
   if (
     message.content.startsWith('i!') ||
     message.author.bot ||
@@ -58,7 +52,11 @@ client.on('messageCreate', async (message) => {
     return
   }
   let conversationLog = [
-    { role: 'system', content: 'you are a informational chatbot.' },
+    {
+      role: 'system',
+      content:
+        'You are a header generator chatbot. You summerise text sent by the user into a concise, simple, and neutral half sentence to be used as a topic header for the message. The header is always less than 35 characters. The header is general, not specific. The header should not be wrapped in any quotations. The header does not try to answer the question or text.',
+    },
   ]
   await message.channel.sendTyping()
 
